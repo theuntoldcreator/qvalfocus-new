f20596ad84a0bc3f91daff9a434dbfb2903ffce5
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-11 01:30:19 -0500

    [dyad] Fixed JSX typo in application form - wrote 1 file(s)

--- client/src/components/job/apply-form.tsx
+++ client/src/components/job/apply-form.tsx
@@ -129,7 +129,7 @@ export function ApplyForm({ job }: ApplyFormProps) {
               <>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <FormField control={form.control} name="firstName" render={({ field }) => ( <FormItem><FormLabel>First Name *</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>)} />
-                  <FormField control={form.control} name="lastName" render={({ field }) => ( <FormItem><FormLabel>Last Name *</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormMessage>)} />
+                  <FormField control={form.control} name="lastName" render={({ field }) => ( <FormItem><FormLabel>Last Name *</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>)} />
                 </div>
                 <FormField control={form.control} name="email" render={({ field }) => ( <FormItem><FormLabel>Email *</FormLabel><FormControl><Input type="email" {...field} /></FormControl><FormMessage /></FormItem>)} />
                 <FormField control={form.control} name="phone" render={({ field }) => ( <FormItem><FormLabel>Phone</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>)} />
