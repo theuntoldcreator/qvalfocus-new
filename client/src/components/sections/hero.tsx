ba66b68945eb202595191741f5e1a70600f33470
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-15 20:30:30 -0500

    Reverted all changes back to version dc4189d3d4a4b416c3f659440310c3fcd011dfc2

--- client/src/components/sections/hero.tsx
+++ client/src/components/sections/hero.tsx
@@ -1,6 +1,6 @@
 export function Hero() {
   return (
-    <section className="pt-24 pb-12 bg-[#e5e5e5]">
+    <section className="pt-24 pb-12 bg-slate-50">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid lg:grid-cols-2 gap-8">
           {/* Left Layout: Text Content */}
