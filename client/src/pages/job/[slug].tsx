742bec7e0312d41f4eb015340e167a54ac991b46
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-16 01:47:44 -0500

    [dyad] Increased form width on job page - wrote 1 file(s)

--- client/src/pages/job/[slug].tsx
+++ client/src/pages/job/[slug].tsx
@@ -67,11 +67,11 @@ export default function JobPage() {
               </div>
             </div>
           ) : job ? (
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
-              <div className="lg:col-span-2">
+            <div className="grid grid-cols-1 lg:grid-cols-5 gap-12">
+              <div className="lg:col-span-3">
                 <JobDetails job={job} />
               </div>
-              <div className="lg:col-span-1">
+              <div className="lg:col-span-2">
                 <div className="sticky top-28">
                   <ApplyForm job={job} />
                 </div>
