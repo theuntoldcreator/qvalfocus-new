1c6982ccfa5a0fb5c7eba186e4e8c9ff6a7950ed
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-22 18:27:26 -0500

    [dyad] Update button color in header - wrote 1 file(s)

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -174,7 +174,7 @@ export function Header({ onToggleMobileMenu }: HeaderProps) {
               </nav>
 
               <div className="flex items-center space-x-4 ml-8">
-                <Button asChild className="hidden md:inline-flex">
+                <Button asChild className="hidden md:inline-flex bg-[#264AB8] hover:bg-[#203C9A]">
                   <Link to="/contact?type=client">
                     Hire A Talent <ArrowUpRight className="ml-2 h-4 w-4" />
                   </Link>
