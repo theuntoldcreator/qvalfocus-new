599c24229456ed3483b5ed0d677baa9f9332c643
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-24 15:05:46 -0500

    [dyad] Fixed the broken link to the Careers page - wrote 2 file(s)

---
+++ client/src/pages/Careers.tsx
@@ -0,0 +1,21 @@
+import { PageHeader } from "@/components/layout/page-header";
+import { JobList } from "@/components/sections/job-list";
+
+export default function Careers() {
+  return (
+    <>
+      <PageHeader
+        title="Careers"
+        breadcrumbItems={[
+          { name: "Home", href: "/" },
+          { name: "Careers" },
+        ]}
+      />
+      <main className="py-12 md:py-20">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <JobList />
+        </div>
+      </main>
+    </>
+  );
+}
