ece97876de99c4a84489d3523be65739e6604e78
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-24 15:10:29 -0500

    [dyad] Fixed 15 TypeScript compilation errors - wrote 4 file(s)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layout/layout";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-import JobDetail from "./pages/JobDetail";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfService from "./pages/TermsOfService";
-import Apply from "./pages/Apply";
-import Careers from "./pages/Careers";
+import Index from "./pages/index";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import BlogPost from "./pages/blog-post";
+import JobDetail from "./pages/job-detail";
+import PrivacyPolicy from "./pages/privacy-policy";
+import TermsOfService from "./pages/terms-of-service";
+import Apply from "./pages/apply";
+import Careers from "./pages/careers";
 
 function App() {
   return (
